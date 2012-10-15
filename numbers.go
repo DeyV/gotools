0be9037efc1372b27fc559d8ee2d788fe498cb26
Dawid Polak <dawid@mentax.pl>
2012-10-15 17:02:16 +0200

    typing problems

--- numbers.go
+++ numbers.go
@@ -2,9 +2,30 @@ package gotools
 
 import (
 	"fmt"
+	"strings"
 )
 
 func NumerFormat(number interface{}, decimals int, decPoint, thousandsSep string) (string, error) {
+	var num float64
+	var format string
+
+	if decimals > 0 {
+		format = fmt.Sprint("%%.%df", decimals)
+	} else {
+		format = "%f"
+	}
+
+	num, _ = caseToFloat(number)
+
+	if thousandsSep == "" {
+		out, _ := fmt.Sprintf(format, num), nil
+		return strings.Replace(out, ".", decPoint, 1), nil
+	}
+
+	return fmt.Sprintf(format, num), nil
+}
+
+func caseToFloat(number interface{}) (float64, error) {
 	var num float64
 
 	switch i := number.(type) {
@@ -15,13 +36,5 @@ func NumerFormat(number interface{}, decimals int, decPoint, thousandsSep string
 	case nil:
 		num = 0
 	}
-
-	var format string
-	if decimals > 0 {
-		format = fmt.Sprint("%%.%df", decimals)
-	} else {
-		format = "%f"
-	}
-
-	return fmt.Sprintf(format, num), nil
+	return num, nil
 }
